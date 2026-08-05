5c05a594dad3558763eea4527d0e6990a85534f3
2026csm1021 <2026csm1021@iitrpr.ac.in>
2026-08-05 14:40:47 +0530

    make_csr

--- LAB_ASSIGNMENT_01/make_csr.cpp
+++ LAB_ASSIGNMENT_01/make_csr.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 
 struct edge
@@ -10,80 +8,119 @@ struct edge
     int weight;
 };
 
+void BFS(int source,
+         vector<int> &row_ptr,
+         vector<int> &col_idx,
+         int V)
+{
+    vector<bool> visited(V, false);
+    queue<int> q;
+
+    visited[source] = true;
+    q.push(source);
+
+    cout << "\nBFS Traversal : ";
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        cout << u << " ";
+
+        for (int i = row_ptr[u]; i < row_ptr[u + 1]; i++)
+        {
+            int v = col_idx[i];
+
+            if (!visited[v])
+            {
+                visited[v] = true;
+                q.push(v);
+            }
+        }
+    }
+
+    cout << endl;
+}
+
+
 int main()
 {
     vector<edge> edges;
     edge e;
 
-    int V,E;
+    int V, E;
 
     FILE *input_file = fopen("input.txt", "r");
+
     if (input_file == NULL)
     {
-        cout << "[ERROR] Can't input file " << endl;
+        cout << "[ERROR] Can't open input file." << endl;
         return 1;
     }
 
-    fscanf(input_file,"%d %d",&V,&E);
+    fscanf(input_file, "%d %d", &V, &E);
 
-    while (fscanf(input_file, "%d %d %d", &e.source, &e.destination, &e.weight) == 3)
+    while (fscanf(input_file, "%d %d %d",
+                  &e.source,
+                  &e.destination,
+                  &e.weight) == 3)
     {
         edges.push_back(e);
-
     }
 
+    fclose(input_file);
 
-    vector<int> count(V, 0);
+    // Sort according to source vertex
+    sort(edges.begin(), edges.end(), [](edge a, edge b)
+    {
+        if (a.source == b.source)
+            return a.destination < b.destination;
 
-    for (auto &e : edges)
-        count[e.source]++;
+        return a.source < b.source;
+    });
 
-    vector<int> row_ptr(V + 1);
-
-    row_ptr[0] = 0;
-
-    for (int i = 1; i <= V; i++)
-        row_ptr[i] = row_ptr[i - 1] + count[i - 1];
-
-
-
+    vector<int> row_ptr(V + 1, 0);
     vector<int> col_idx;
     vector<int> values;
 
+    int currentVertex = 0;
+
     for (auto &e : edges)
     {
+        while (currentVertex < e.source)
+        {
+            row_ptr[currentVertex + 1] = col_idx.size();
+            currentVertex++;
+        }
+
         col_idx.push_back(e.destination);
         values.push_back(e.weight);
     }
 
-    // fclose(input_file);
-    // int i = 0;
-    // auto temp = edges[0];
-    // row_ptr.push_back(i);
+    while (currentVertex < V)
+    {
+        row_ptr[currentVertex + 1] = col_idx.size();
+        currentVertex++;
+    }
 
-    // for (auto &a : edges)
-    // {
-    //     if (a.source != temp.source)
-    //     {
-    //         temp = a;
-    //         row_ptr.push_back(i);
-    //     }
-    //     col_ptr.push_back(a.destination);
-    //     values.push_back(a.weight);
-    //     i++;
-    // }
-
-    cout << "COL_PTR : ";
-    for (auto a : col_idx)
-        cout << a << " ";
-    cout << endl;
     cout << "ROW_PTR : ";
-    for (auto a : row_ptr)
-        cout << a << " ";
-    cout << endl;
-    cout << "Values : ";
-    for (auto a : values)
-        cout << a << " ";
+    for (int x : row_ptr)
+        cout << x << " ";
 
     cout << endl;
+
+    cout << "COL_IDX : ";
+    for (int x : col_idx)
+        cout << x << " ";
+
+    cout << endl;
+
+    cout << "VALUES : ";
+    for (int x : values)
+        cout << x << " ";
+
+    cout << endl;
+
+    return 0;
 }
